7cc0e4b0e5a90b6d4f6a7813eb43efdc429870d2
Tristan <thrawst88@gmail.com>
2015-05-05 13:15:12 -0600

    tests for jsString and fixed some issued with it

--- jsSwift/jsString.swift
+++ jsSwift/jsString.swift
@@ -3,24 +3,44 @@ import Foundation
 extension String {
     var length:Int {get { return count(self) }}
 
-    func substring(start:Int, end:Int) -> String {
-        if end >= start {
+    func substring(start:Int, var end:Int = -1) -> String {
+        if end < 0 {
+            end = count(self)
+        }
+        else {
+            end = min(end, count(self))
+        }
+        
+        if start >= end {
             return ""
         }
+        
         let startIndex:String.Index = advance(self.startIndex, start)
         let endIndex:String.Index = advance(self.endIndex, -(count(self)-end))
         let range:Range<String.Index> = Range<String.Index>(start: startIndex, end: endIndex)
         return self.substringWithRange(range)
     }
     
-    func substr(start:Int, end:Int) -> String {
+    func substr(start:Int, var end:Int = -1) -> String {
+        if end < 0 {
+            end = count(self)
+        }
         return self.substring(start, end: end)
     }
     
-    func indexOf(str:String, start:Int = 0) -> Int {
+    func indexOf(str:String, var start:Int = 0) -> Int {
         var out = -1
+        
+        if start > count(self) {
+            return out
+        }
+        else if start < 0 {
+            start = 0
+        }
+        
         let startIndex:String.Index = advance(self.startIndex, start)
         if let strRange:Range<String.Index> = self.rangeOfString(str, range:Range<String.Index>(start: startIndex, end:self.endIndex)) {
+            out = 0
             for var i:String.Index = self.startIndex; i != strRange.startIndex; i = i.successor() {
                 out += 1
             }
@@ -33,7 +53,7 @@ extension String {
     }
 
     mutating func trim() {
-        self.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+        self = self.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
     }
     
     func charAt(loc:Int) -> Character? {
--- jsSwiftTests/jsStringTests.swift
+++ jsSwiftTests/jsStringTests.swift
@@ -20,8 +20,65 @@ class jsStringTests: XCTestCase {
     }
     
     func testLength() {
-        // This is an example of a functional test case.
-        XCTAssertEqual("hello".length, 5, "Pass")
+        XCTAssertEqual("hello".length, 5, "string length incorrect")
     }
     
+    func testSubstring() {
+        let str:String = "hello"
+        XCTAssert(str.substr(0) == "hello", "fails with 1 argument")
+        XCTAssert(str.substring(2) == "llo", "fails with 1 argument")
+        
+        XCTAssert(str.substr(0, end: 2) == "he", "expected \"he\" got \(str.substr(0,end: 2))")
+        XCTAssert(str.substring(0, end: 2) == "he", "expected \"he\" got \(str.substring(0, end:2))")
+        
+        XCTAssert(str.substr(0, end: 10) == str, "fails when end > str.length")
+        XCTAssert(str.substring(0, end: 10) == str, "fails when end > str.length")
+        
+        XCTAssert(str.substring(5, end: 2) == "", "substring fails when start > end")
+    }
+    
+    func testIndexOf() {
+        let str:String = "justice"
+        let tok:String = "s"
+        XCTAssert(str.indexOf("z") == -1, "z in \(str)")
+        XCTAssert(str.indexOf(tok) == 2, "indexOf returns wrong index of character: \(str), expected: s, got: \(str.indexOf(tok))")
+        XCTAssert(str.indexOf(tok, start: 3) == -1, "indexOf returns wrong value when start defined")
+        XCTAssert(str.indexOf(tok, start: 20) == -1, "indexOf returns wrong value when start > str.length")
+    }
+    
+    func testSplit() {
+        let str:String = "razzmatazz"
+        XCTAssertEqual(str.split("zz"), ["ra", "mata", ""], "string split incorrectly")
+    }
+    
+    func testTrim() {
+        var str:String = "  hello \n"
+        str.trim()
+        XCTAssert( str == "hello", "string trimmed incorrectly, \"\(str)\"")
+    }
+    
+    func testCharAt() {
+        let str:String = "liberty"
+        XCTAssert(str.charAt(0) == Character("l"), "wrong character grabbed with charAt(int)")
+        XCTAssert(str[0] == Character("l"), "wrong character grabbed with str[int]")
+        
+        XCTAssert(str.charAt(count(str)-1) == Character("y"), "wrong character grabbed with str.charAt(str.length-1)")
+        XCTAssert(str[count(str)-1] == Character("y"), "wrong character grabbed with str[str.length-1]")
+        
+        XCTAssert(str.charAt(-1) == nil, "wrong character grabbed with charAt(-1)")
+        XCTAssert(str[-1] == nil, "wrong character grabbed with str[-1]")
+        
+        XCTAssert(str.charAt(20) == nil, "wrong character grabbed with charAt(20)")
+        XCTAssert(str[20] == nil, "wrong character grabbed with str[20]")
+    }
+    
+    func testToUpperCase() {
+        let str:String = "freedom"
+        XCTAssert(str.toUpperCase() == str.uppercaseString, "uppercase is wrong: \(str) -> \(str.toUpperCase())")
+    }
+    
+    func testToLowerCase() {
+        let str:String = "BRAVERY"
+        XCTAssert(str.toLowerCase() == str.lowercaseString, "lowercase is wrong: \(str) -> \(str.toLowerCase())")
+    }
 }
