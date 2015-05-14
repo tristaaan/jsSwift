28fd0fec15d1485f6422fe3749ac46365d715f9d
Tristan <thrawst88@gmail.com>
2015-05-14 12:01:37 -0600

    new jsDate functions, getYear->getFullYear

--- jsSwift/jsDate.swift
+++ jsSwift/jsDate.swift
@@ -10,7 +10,7 @@ import Foundation
 
 extension NSDate {
     
-    convenience init(year:Int=0, month:Int=0, day:Int=0, hour:Int=0, minute:Int=0, seconds:Int=0) {
+    convenience init(year:Int, month:Int, day:Int=0, hour:Int=0, minute:Int=0, seconds:Int=0) {
         let comp:NSDateComponents = NSDateComponents()
         comp.year  = year
         comp.month = month
@@ -28,46 +28,68 @@ extension NSDate {
         return cal
     }
     
+    //MARK: Getters
     func now() -> Double {
         return NSTimeIntervalSince1970
     }
     
     func getSeconds() -> Int {
-        let comp:NSDateComponents = localeCal().components(NSCalendarUnit.CalendarUnitSecond, fromDate: self)
+        let comp:NSDateComponents = self.localeCal().components(NSCalendarUnit.CalendarUnitSecond, fromDate: self)
         return comp.second
     }
     
     func getMinutes() -> Int {
-        let comp:NSDateComponents = localeCal().components(NSCalendarUnit.CalendarUnitMinute, fromDate: self)
+        let comp:NSDateComponents = self.localeCal().components(NSCalendarUnit.CalendarUnitMinute, fromDate: self)
         return comp.minute
     }
     
     func getHours() -> Int {
-        let comp:NSDateComponents = localeCal().components(NSCalendarUnit.CalendarUnitHour, fromDate: self)
+        let comp:NSDateComponents = self.localeCal().components(NSCalendarUnit.CalendarUnitHour, fromDate: self)
         return comp.hour
     }
     
     func getDate() -> Int {
-        let comp:NSDateComponents = localeCal().components(NSCalendarUnit.CalendarUnitDay, fromDate: self)
+        let comp:NSDateComponents = self.localeCal().components(NSCalendarUnit.CalendarUnitDay, fromDate: self)
         return comp.day
     }
     
     func getDay() -> Int {
-        let comp:NSDateComponents = localeCal().components(NSCalendarUnit.CalendarUnitWeekday, fromDate: self)
+        let comp:NSDateComponents = self.localeCal().components(NSCalendarUnit.CalendarUnitWeekday, fromDate: self)
         return comp.weekday
     }
     
     func getMonth() -> Int {
-        let comp:NSDateComponents = localeCal().components(NSCalendarUnit.CalendarUnitMonth, fromDate: self)
+        let comp:NSDateComponents = self.localeCal().components(NSCalendarUnit.CalendarUnitMonth, fromDate: self)
         return comp.month
     }
     
-    func getYear() -> Int {
-        let comp:NSDateComponents = localeCal().components(NSCalendarUnit.CalendarUnitYear, fromDate: self)
+    func getFullYear() -> Int {
+        let comp:NSDateComponents = self.localeCal().components(NSCalendarUnit.CalendarUnitYear, fromDate: self)
         return comp.year
+    }
+    
+    //MARK: Setters
+    
+    //MARK: Conversion Getters
+    func toLocaleString() -> String {
+        return self.descriptionWithLocale(NSLocale.currentLocale())!
     }
     
     func toString() -> String {
         return self.description
+    }
+    
+    func toDateString() -> String {
+        let formatter = NSDateFormatter()
+        formatter.dateStyle = .FullStyle
+        formatter.timeStyle = .NoStyle
+        return formatter.stringFromDate(self)
+    }
+    
+    func toTimeString() -> String {
+        let formatter = NSDateFormatter()
+        formatter.dateStyle = .NoStyle
+        formatter.timeStyle = .LongStyle
+        return formatter.stringFromDate(self)
     }
 }
--- jsSwiftTests/jsDateTests.swift
+++ jsSwiftTests/jsDateTests.swift
@@ -20,12 +20,12 @@ class jsDateTests: XCTestCase {
     }
     
     func testNow() {
-        XCTAssert(NSDate(year:1970).now() == NSTimeIntervalSince1970, "incorrect time since 1970")
+        XCTAssert(NSDate(year:1970, month:1).now() == NSTimeIntervalSince1970, "incorrect time since 1970")
     }
     
     func testGetters() {
         let date:NSDate = NSDate(year: 2007, month: 1, day: 8, hour: 9, minute: 43, seconds: 24)
-        XCTAssert(date.getYear()  == 2007, "year getter wrong: \(date.getYear())")
+        XCTAssert(date.getFullYear()  == 2007, "year getter wrong: \(date.getFullYear())")
         XCTAssert(date.getMonth() == 1,    "month getter wrong: \(date.getMonth())")
         XCTAssert(date.getDate()   == 8,   "date getter wrong: \(date.getDate())")
         XCTAssert(date.getDay()   == 2,   "day getter wrong: \(date.getDay())") // was a Monday
@@ -37,5 +37,15 @@ class jsDateTests: XCTestCase {
     func testToString(){
         let date:NSDate = NSDate(year: 1984, month: 1, day: 24, hour: 0, minute: 0, seconds: 0)
         XCTAssert(date.toString() == date.description,    "toString")
+    }
+    
+    func testToDateString() {
+        let date:NSDate = NSDate(year: 2014, month: 6, day: 2, hour: 11, minute: 45, seconds: 0)
+        XCTAssert(date.toDateString() == "Monday, June 2, 2014", "Date string wrong")
+    }
+    
+    func testToTimeString() {
+        let date:NSDate = NSDate(year: 2014, month: 6, day: 2, hour: 11, minute: 45, seconds: 0)
+        XCTAssert(date.toTimeString() == "11:45:00 AM MDT", "Time string wrong")
     }
 }
